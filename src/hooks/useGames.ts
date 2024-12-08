c0d51f5b06988136eea918b35ed35efc186a98b5
Jeremy Stuck <jeremy.stuck@gmail.com>
2024-12-07 16:39:18 -0800

    Ship genres with the app

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,9 +11,9 @@ export interface Platform {
 export interface Game {
   id: number;
   name: string;
-  background_image: string,
-  parent_platforms: { platform: Platform }[],
-  metacritic: number
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 const useGames = (gameQuery: GameQuery) => 
